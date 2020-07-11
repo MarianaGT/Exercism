ae95310287c04a1acbd0557ba54a69a5a31fb3b9
MarianaGT <64441365+MarianaGT@users.noreply.github.com>
2020-07-11 12:15:17 -0500

    Update

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,11 +1,10 @@
 class Complement
-  def of_dna(dna)
+  def of_dna(dna='')
     rna = {
       'U' => 'A',
       'G' => 'C',
       'C' => 'G',
       'A' => 'T',
-      ''  => ''
     }
 
     dna.gsub(/\w/, rna)
