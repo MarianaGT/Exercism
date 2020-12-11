f2ec6310367012c4b206433ba5a9bae6ca110ee5
Mariana Galan <gatm870125@gmail.com>
2020-12-11 17:37:56 -0600

    challenge 7

--- javascript/challenges.js
+++ javascript/challenges.js
@@ -27,3 +27,9 @@ function findPerimeter(length, width) {
 function minMax(arr) {
   return [Math.min(...arr), Math.max(...arr)];
 }
+
+/* You will be given two extremely similar arrays, but exactly one of the items in an array will be valued slightly higher than its counterpart (which means that evaluating the value > the other value will return true).
+Create a function that returns whether the first array is slightly superior to that of the second. */
+function isFirstSuperior(arr1, arr2) {
+  return arr1.join() > arr2.join() === true;
+}
