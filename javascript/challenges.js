159f43ca74213b05d207ad2fad59d8fd18f6c17e
Mariana Galan <gatm870125@gmail.com>
2020-12-11 17:12:16 -0600

    challenge 5

--- javascript/challenges.js
+++ javascript/challenges.js
@@ -13,7 +13,12 @@ function howManySeconds(hours) {
   return hours * 3600;
 }
 
+//Write a function that takes an integer minutes and converts it to seconds
+function convert(minutes) {
+  return minutes * 60;
+}
+
 // Create a function that takes length and width and finds the perimeter of a rectangle.
 function findPerimeter(length, width) {
-  return (length * 2) + (width * 2)
+  return (length * 2) + (width * 2);
 }
