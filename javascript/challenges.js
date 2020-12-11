dd48c66b412fd117fbdbc0d91e075e0c2d8bf882
Mariana Galan <gatm870125@gmail.com>
2020-12-11 17:09:22 -0600

    challenge 4

--- javascript/challenges.js
+++ javascript/challenges.js
@@ -12,3 +12,8 @@ function addition(a, b) {
 function howManySeconds(hours) {
   return hours * 3600;
 }
+
+// Create a function that takes length and width and finds the perimeter of a rectangle.
+function findPerimeter(length, width) {
+  return (length * 2) + (width * 2)
+}
