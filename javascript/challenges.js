1c3d60bfa2118d5200780089056d223f39576d0a
Mariana Galan <gatm870125@gmail.com>
2020-12-11 17:19:47 -0600

    challenge 6

--- javascript/challenges.js
+++ javascript/challenges.js
@@ -22,3 +22,8 @@ function convert(minutes) {
 function findPerimeter(length, width) {
   return (length * 2) + (width * 2);
 }
+
+//Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
+function minMax(arr) {
+  return [Math.min(...arr), Math.max(...arr)];
+}
