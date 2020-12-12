3b9df974b0ea20deb4a563f4bbcf2367b67cccc3
Mariana Galan <gatm870125@gmail.com>
2020-12-11 18:16:18 -0600

    challenge 8 modified

--- javascript/challenges.js
+++ javascript/challenges.js
@@ -46,10 +46,11 @@ class Person {
 
   compareAge(other) {
     if (this.age > other.age) {
-      other.name + " is younger than me";
+      return `${other.name} is younger than me.`;
     } else if (this.age < other.age) {
-      other.name +  " is older than me";
+      return `${other.name} is older than me.`;
     } else {
-      other.name + " is the same age as me";
+      return `${other.name} is the same age as me.`;
     }
   }
+}
