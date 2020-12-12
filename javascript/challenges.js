6cc3b1752a1a1874b2b2c7b632a120afa1898089
Mariana Galan <gatm870125@gmail.com>
2020-12-11 18:11:06 -0600

    challenge 8

--- javascript/challenges.js
+++ javascript/challenges.js
@@ -33,3 +33,23 @@ Create a function that returns whether the first array is slightly superior to t
 function isFirstSuperior(arr1, arr2) {
   return arr1.join() > arr2.join() === true;
 }
+
+/* Create a method in the Person class which returns how another person's age compares.
+Given the instances p1, p2 and p3, which will be initialised with the attributes name and age,
+return a sentence in the following format:
+{other person name} is {older than / younger than / the same age as} me.*/
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  compareAge(other) {
+    if (this.age > other.age) {
+      other.name + " is younger than me";
+    } else if (this.age < other.age) {
+      other.name +  " is older than me";
+    } else {
+      other.name + " is the same age as me";
+    }
+  }
