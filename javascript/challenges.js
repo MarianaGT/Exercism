58aeb931caf32d1b13f1fe2ab40cd9cabcf008d2
Mariana Galan <gatm870125@gmail.com>
2020-12-13 08:29:26 -0600

    challenge 10

--- javascript/challenges.js
+++ javascript/challenges.js
@@ -61,3 +61,19 @@ and false otherwise. Keep in mind JavaScript's Date month is 0 based, meaning De
 function timeForMilkAndCookies(date) {
   return (date.getMonth() === 11) && (date.getDate() === 24);
 }
+
+/* Your function will be passed two functions, f and g, that don't take any parameters. Your function has to call them, and return a string which indicates which function returned the larger number.
+
+If f returns the larger number, return the string f.
+If g returns the larger number, return the string g.
+If the functions return the same number, return the string neither.
+Introduction to high order functions*/
+function whichIsLarger(f, g) {
+  if (f() > g()){
+    return 'f';
+  } else if (f() < g()) {
+    return 'g';
+  } else {
+    return 'neither';
+  }
+}
