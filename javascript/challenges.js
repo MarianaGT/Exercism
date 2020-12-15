980d85e7d05b6ccf3659181e4e754bb20dfa647b
Mariana Galan <gatm870125@gmail.com>
2020-12-15 08:13:30 -0600

    challenge 12 solution 2

--- javascript/challenges.js
+++ javascript/challenges.js
@@ -96,5 +96,5 @@ function countTrue(arr) {
 
 /* Create a function that will take a HEX number and returns the binary equivalent (as a string).*/
 function toBinary(num) {
-  return (num >>> 0).toString(2);
+  return num.toString(2);
 }
