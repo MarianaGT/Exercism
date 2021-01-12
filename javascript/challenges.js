c9711cb938382594fb82260d37b120b6947a3a35
Mariana Galan <gatm870125@gmail.com>
2021-01-12 09:39:00 -0600

    reserved keywords

--- javascript/challenges.js
+++ javascript/challenges.js
@@ -144,3 +144,10 @@ console.log(null || "user") → user
 let one = 1, two = 2;
 console.log(one + two);
 // → 3
+
+/*Reserved keywords
+break case catch class const continue debugger default
+delete do else enum export extends false finally for
+function if implements import interface in instanceof let
+new package private protected public return static super
+switch this throw true try typeof var void while with yield*/
