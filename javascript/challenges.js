6c478065afda7bfe5bad43d969bd330d62c07ec5
Mariana Galan <gatm870125@gmail.com>
2021-01-12 10:13:43 -0600

    chain conditional statement proper syntax

--- javascript/challenges.js
+++ javascript/challenges.js
@@ -158,3 +158,14 @@ if (!Number.isNaN(theNumber)) {
   console.log("Your number is the square root of " +
               theNumber * theNumber);
 }
+
+// Chain conditional statements proper syntax
+let num = Number(prompt("Pick a number"));
+
+if (num < 10) {
+  console.log("Small");
+} else if (num < 100) {
+  console.log("Medium");
+} else {
+  console.log("Large");
+}
