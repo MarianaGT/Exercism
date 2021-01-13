80484e2d5ff399d99dd19b3cd31cbd90b94d77af
Mariana Galan <gatm870125@gmail.com>
2021-01-13 09:54:38 -0600

    FizzBuzz challenge

--- javascript/challenges.js
+++ javascript/challenges.js
@@ -245,3 +245,22 @@ let result = '#';
 for (let counter = 0; counter < 7; counter += 1) {
   console.log(result); result += '#';
 }
+
+/*Write a program that uses console.log to print all the numbers from 1 to 100,
+with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number,
+and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+
+When you have that working, modify your program to print "FizzBuzz" for numbers
+that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers
+divisible by only one of those). */
+for (let num = 1; num <= 100; num += 1) {
+  if (num % 3 == 0 && num % 5 == 0) {
+    console.log("FizzBuzz");
+  } else if (num % 3 == 0) {
+    console.log("Fizz")
+  } else if (num % 5 == 0) {
+    console.log("Buzz");
+  } else {
+    console.log(num);
+  }
+}
