d4b49cac2583d9bdeec8149647e677fbb506d7d2
Mariana Galan <gatm870125@gmail.com>
2021-01-14 08:50:51 -0600

    Chessboard challenge 01

--- javascript/challenges.js
+++ javascript/challenges.js
@@ -264,3 +264,12 @@ for (let num = 1; num <= 100; num += 1) {
     console.log(num);
   }
 }
+
+/*Write a program that creates a string that represents an 8×8 grid, using newline
+characters to separate lines. At each position of the grid there is either a space
+or a "#" character. The characters should form a chessboard.
+Passing this string to console.log should show something like this: */
+let grid = '' + '#';
+for (let size = 0; size < 8; size += 1) {
+  console.log(grid * 4);
+}
