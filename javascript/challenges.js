47ce352b39a9ef729fa43d81735fef0f32855d87
Mariana Galan <gatm870125@gmail.com>
2021-01-15 10:47:32 -0600

    Function declaration 01

--- javascript/challenges.js
+++ javascript/challenges.js
@@ -299,3 +299,15 @@ let size = 8
 for (let grid = " " + "#"; grid.length > size; grid + "\n") {
   console.log(grid.repeat(size * size));
 }
+
+/* Function declarations are not part of the regular top-to-bottom flow of control.
+They are conceptually moved to the top of their scope and can be used by all the
+code in that scope. This is sometimes useful because it offers the freedom to order
+code in a way that seems meaningful, without worrying about having to define all
+functions before they are used.
+*/
+console.log("The future says:", future());
+
+function future() {
+  return "You'll never have flying cars";
+}
