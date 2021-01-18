791b94b32112e3c8bd8dc884024c7514ea0d0806
Mariana Galan <gatm870125@gmail.com>
2021-01-18 13:12:20 -0600

    psuh and pop methods

--- javascript/challenges.js
+++ javascript/challenges.js
@@ -373,3 +373,13 @@ a number that indicates how many uppercase “B” characters there are in the s
 function countBs(string) {
   return string.match(/B/g).length;
 }
+
+/* The push method adds values to the end of an array, and the pop method does
+the opposite, removing the last value in the array and returning it.
+*/
+let sequence = [1, 2, 3];
+sequence.push(4);
+sequence.push(5);
+console.log(sequence); // → [1, 2, 3, 4, 5]
+console.log(sequence.pop()); // → 5
+console.log(sequence); // → [1, 2, 3, 4]
