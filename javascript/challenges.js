8285d8a73390053f6104e6bd34048ce8ff45c0db
Mariana Galan <gatm870125@gmail.com>
2021-01-28 09:17:36 -0600

    squirrel transformations

--- javascript/challenges.js
+++ javascript/challenges.js
@@ -407,3 +407,20 @@ let journal = [
             "beer"],
    squirrel: true},
 ];
+
+/* To extract a two-by-two table for a specific event from the journal, we must
+loop over all the entries and tally how many times the event occurs in relation
+to squirrel transformations.
+Arrays have an includes method that checks whether a given value exists in the array.
+*/
+
+function tableFor(event, journal) {
+  let table = [0, 0, 0, 0];
+  for (let i = 0; i < journal.length; i++) {
+    let entry = journal[i], index = 0;
+    if (entry.events.includes(event)) index += 1;
+    if (entry.squirrel) index += 2;
+    table[index] += 1;
+  }
+  return table;
+}
